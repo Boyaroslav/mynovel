29a448dbcbea41116dc11dbaad256b5821ac1e6c
Boyaroslav <boyara.best@gmail.com>
2026-03-29 15:03:29 +0200

    license and readme added

---
+++ README.md
@@ -0,0 +1,5 @@
+
+
+<h1>CCN or mynovel</h1>
+<p>This project is licensed under the GNU General Public License v3.0.</p>
+<p>This code is going to be used in the Cuck and Cock visual novel game</p>
--- scene.hpp
+++ scene.hpp
@@ -1,3 +1,19 @@
+/*
+ * This file is part of CCN (mynovel).
+ * Copyright (C) 2026 Iaroslav Bobylev
+ * CCN (mynovel) is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, version 3 of the License.
+ *
+ * CCN (mynovel) is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with CCN (mynovel). If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include "utils.hpp"
 #include "event.hpp"
 #include <cstring>
